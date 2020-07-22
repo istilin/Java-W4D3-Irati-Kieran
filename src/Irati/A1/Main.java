c67e33786aa9153acad62fd2abda1daca4548655
Irati Larreina <ilarreina_6@gotmail.com>
2020-07-22 09:56:47 +0200

    A1 finished

--- src/Irati/A1/Main.java
+++ src/Irati/A1/Main.java
@@ -1,4 +1,20 @@
 package Irati.A1;
 
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) {
+        try{
+            File file = new File("src/Irati/A1/my_files/myFile.txt");
+            if (file.createNewFile()){
+                System.out.println("File created!");
+            } else {
+                System.out.println("File already exists");
+            }
+        } catch(Exception e){
+            System.out.println("Error");
+            e.printStackTrace();
+        }
+    }
+
 }
