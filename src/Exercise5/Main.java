a7347c17f26c3d158f066f46a3d69a78845b0133
aki <kieran.mortimer@gmail.com>
2020-07-22 10:52:30 +0200

    Exercise ->5

--- src/Exercise5/Main.java
+++ src/Exercise5/Main.java
@@ -1,4 +1,52 @@
 package Exercise5;
 
+import Exercise5.classes.Product;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class Main {
+    public static void printReport(HashMap<Integer, Product> stockHashMap){
+        final String workingDir = System.getProperty("user.dir");
+        final String reportName = "report.txt";
+
+        try {
+            File file = new File(workingDir + File.separator + reportName);
+
+            file.createNewFile();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Product p : stockHashMap.values()) {
+                stringBuilder.append(p.toString());
+                stringBuilder.append("\n");
+            }
+
+            FileWriter writer = new FileWriter(file);
+            writer.write(stringBuilder.toString());
+            writer.close();
+
+            System.out.println("Report has been created");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        HashMap<Integer, Product> shopStock = new HashMap<>();
+        shopStock.put(0, new Product("Voeslauer sparkling water", 1.99, 10, "It's wet"));
+        shopStock.put(1, new Product("Ice Tea", 2.50, 10, "Has some lemon in it"));
+
+        printReport(shopStock);
+
+        // Exercise 6
+        File file = new File(System.getProperty("user.dir") + File.separator + "report.txt");
+
+        // Read text file as string
+
+
+    }
 }
--- src/Exercise5/classes/Product.java
+++ src/Exercise5/classes/Product.java
@@ -1,4 +1,52 @@
 package Exercise5.classes;
 
 public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+    private String description;
+
+    public Product(String name, double price, int quantity, String description) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s | %s | %s | %s", this.name, this.price + "€", this.quantity + "stk.", this.description);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
